03a790b91aec33c51327b99762f881e80e73b27e
huangbixia <15220007752@163.com>
2020-03-22 15:20:57 +0800

    <ADD>add new code

---
+++ angular2Reddit/src/app/article/article.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './articel.model';
+
+@Component({
+  selector: 'app-article',
+  templateUrl: './article.component.html',
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
+})
+export class ArticleComponent implements OnInit {
+  @Input() article: Article;
+
+  constructor() { 
+  }
+
+  voteUp() {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown() {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {
+  }
+
+}
